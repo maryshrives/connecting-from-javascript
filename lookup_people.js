830624fe81bd4788b96f82b5ed68480eeead9b67
Mary Shrives <mshrives@alumni.yorku.ca>
2017-12-19 17:03:13 +0000

    refactored version

--- lookup_people.js
+++ lookup_people.js
@@ -10,16 +10,30 @@ const client = new pg.Client({
   port     : settings.port,
   ssl      : settings.ssl
 });
+//show the date without the time afterwards
+function correctDate(date) {
+  let dateObj = new Date(date);
+  let day = dateObj.getUTCDate();
+  let month = dateObj.getUTCMonth() + 1;
+  let year = dateObj.getUTCFullYear();
+  let correctedDate = year + "-" + month + "-" + day;
+  return correctedDate;
+}
+
+function lookUpPeopleByName (name, cb) {
+  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [name], cb)
+}
 
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1", [args[0]], (err, result) => {
+  lookUpPeopleByName (args[0], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows); //output: 1
+    let niceDate = correctDate(result.rows[0].birthdate);
+    console.log(result.rows[0].id + ": " + result.rows[0].first_name + " " + result.rows[0].last_name + ", born " + niceDate);
     client.end();
   });
 });
