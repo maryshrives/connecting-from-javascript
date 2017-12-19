70cd48c1953f207c557f4e220850175f9dda04b3
Mary Shrives <mshrives@alumni.yorku.ca>
2017-12-19 21:15:45 +0000

    committing first migration

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,20 @@
 // Update with your config settings.
 
+const settings = require("./settings");
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password,
+      host: settings.hostname,
+      ssl: settings.ssl
     }
   },
+
 
   staging: {
     client: 'postgresql',
